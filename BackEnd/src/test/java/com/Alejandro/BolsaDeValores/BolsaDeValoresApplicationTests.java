a8de2e573e4a3843309e937678e011175b61d7eb
alejandro <alejandrodtosi@gmail.com>
2026-04-26 05:09:56 -0300

    inicio do projeto

---
+++ BackEnd/src/test/java/com/Alejandro/BolsaDeValores/BolsaDeValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alejandro.BolsaDeValores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaDeValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
